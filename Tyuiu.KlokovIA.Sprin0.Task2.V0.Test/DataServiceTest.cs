204497a5bc06e72955e3b33f8b0b530af590c398
ivan <ivan.proekt00@mail.ru>
2023-09-28 01:09:44 +0500

    Task2

---
+++ Tyuiu.KlokovIA.Sprin0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KlokovIA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KlokovIA.Sprin0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, метод из библиотек
+            var name = "Иван";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Иван", res);
+        }
+    }
+}
---
+++ Tyuiu.KlokovIA.Sprin0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KlokovIA.Sprin0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KlokovIA.Sprin0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83deb31a-69f4-42f6-af19-9f5cc0078be3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

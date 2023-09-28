78e5cdf90901229422b71c52292acfb570506b59
ivan <ivan.proekt00@mail.ru>
2023-09-28 20:08:55 +0500

    Task5

---
+++ Tyuiu.KlokovIA.Sptont0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KlokovIA.Sptont0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KlokovIA.Sptont0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c669aad1-a96d-4b50-ae92-d15f9c7226d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KlokovIA.Sptont0.Task5.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KlokovIA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KlokovIA.Sptont0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример линейной структуры
+            Console.WriteLine(" A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine(" A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine(" A * B = " + DataService.Multiplication(10, 10));
+
+            // Пример развитвляющейся структурынаходится в библиотеке классов Division
+            Console.WriteLine(" A / B = " + DataService.Division(6, 2));
+            Console.ReadKey();
+
+
+        }
+    }
+}
